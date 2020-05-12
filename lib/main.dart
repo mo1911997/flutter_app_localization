1d0dff2c68433ac7eb0cb9407d1273769446ef9e
mo1911997 <mmd26.aka@gmail.com>
2020-05-12 12:07:20 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'AppTranslationsDelegate.dart';
+import 'Application.dart';
+import 'SignUpUI.dart';
+
+Future<Null> main() async {
+  runApp(new LocalisedApp());
+}
+
+class LocalisedApp extends StatefulWidget {
+  @override
+  LocalisedAppState createState() {
+    return new LocalisedAppState();
+  }
+}
+
+class LocalisedAppState extends State<LocalisedApp> {
+  AppTranslationsDelegate _newLocaleDelegate;
+
+  @override
+  void initState() {
+    super.initState();
+    _newLocaleDelegate = AppTranslationsDelegate(newLocale: null);
+    application.onLocaleChanged = onLocaleChange;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpUI(),
+      localizationsDelegates: [
+        _newLocaleDelegate,
+        //provides localised strings
+        GlobalMaterialLocalizations.delegate,
+        //provides RTL support
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale("en", ""),
+        const Locale("mr", ""),
+      ],
+    );
+  }
+
+  void onLocaleChange(Locale locale) {
+    setState(() {
+      _newLocaleDelegate = AppTranslationsDelegate(newLocale: locale);
+    });
+  }
+}
